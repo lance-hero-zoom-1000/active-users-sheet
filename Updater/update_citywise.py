a8a927c4c03960565e1f555ccbc26911b8a15db8
Lawrence Veigas <lawrence.veigas@dineout.co.in>
2022-03-11 14:31:54 +0530

    fixed efunction call. logged_in_active_users was being called twice erroneously

--- Updater/update_citywise.py
+++ Updater/update_citywise.py
@@ -3,19 +3,20 @@ from Helper.DataFetcher import DataFetcher
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+
 def update_citywise(drange, period):
     f = len(drange)
-    
-    for i, d in enumerate(drange):
-        i = i+1
 
-        if i==f:
-            format=True
+    for i, d in enumerate(drange):
+        i = i + 1
+
+        if i == f:
+            format = True
         else:
-            format=False
-        
+            format = False
+
         print(f"Updating for {d}, period: {period}")
-        
+
         df = DataFetcher(d, period=period)
         updater = MetricCalculatorCity(d, period=period)
 
@@ -25,19 +26,27 @@ def update_citywise(drange, period):
         a = updater.calculate_active_users(mau_data=mau_data)
         b = updater.calculate_dp_active_users(mau_data=mau_data)
         c = updater.calculate_loggedin_active_users(mau_data=mau_data)
-        d = updater.calculate_loggedin_active_users(mau_data=mau_data)
+        d = updater.calculate_non_loggedin_active_users(mau_data=mau_data)
         e = updater.calculate_rdp_views_overall(mau_data=mau_data)
 
         # update sheet
         updater.update_sheet(a, MetricCalculatorCity.active_users_dict, format=format)
-        updater.update_sheet(b, MetricCalculatorCity.dp_active_users_dict, format=format)
-        updater.update_sheet(c, MetricCalculatorCity.loggedin_active_users_dict, format=format)
-        updater.update_sheet(d, MetricCalculatorCity.non_loggedin_active_users_dict, format=format)
-        updater.update_sheet(e, MetricCalculatorCity.rdp_viewed_users_dict, format=format)
+        updater.update_sheet(
+            b, MetricCalculatorCity.dp_active_users_dict, format=format
+        )
+        updater.update_sheet(
+            c, MetricCalculatorCity.loggedin_active_users_dict, format=format
+        )
+        updater.update_sheet(
+            d, MetricCalculatorCity.non_loggedin_active_users_dict, format=format
+        )
+        updater.update_sheet(
+            e, MetricCalculatorCity.rdp_viewed_users_dict, format=format
+        )
 
         del df, updater, mau_data
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     yesterday = datetime.today() - relativedelta(days=1)
     update_citywise([yesterday], period="day")
-    
